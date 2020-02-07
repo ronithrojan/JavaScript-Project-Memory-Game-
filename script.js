d0d53c20c023cf6536bcb0dce552bbabf8f4e142
Roniya Rojan <roniyarojan24@gmail.com>
2020-02-07 17:11:16 -0500

    Roniya Commit

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 let array = [
   { image: "./assets/beer.jpg", id: "beer" },
   { image: "./assets/pretzels.jpg", id: "beer" },
-  {
-    image: "assets/Cheese.jpeg",
-    id: "cheese"
-  },
+  { image: "assets/Cheese.jpeg", id: "cheese" },
   {
     image: "assets/Wine.jpeg",
     id: "cheese"
@@ -56,26 +53,53 @@ for (let item of array) {
   let cardContainer = document.createElement("section");
   cardContainer.classList.add("container");
   let card = document.createElement("div");
+  card.setAttribute("data-card-id", item.id);
   card.classList.add("card");
-  let frontImage = document.createElement("img");
-  frontImage.setAttribute("src", item.image);
+  // let frontImage = document.createElement("img");
+  // frontImage.setAttribute("src", item.image);
   let front = document.createElement("div");
-  front.append(frontImage);
+  // front.append(frontImage);
   front.classList.add("front");
+  //back of card images
   let back = document.createElement("div");
   back.classList.add("back");
+  // puts image on back of card.
+  back.style.backgroundImage = `url("${item.image}")`;
+  // image styled within container
+  back.style.backgroundSize = "cover";
+  // rather done thru CSS styling, the three lines of code styled within the container and in the center.
+  back.style.backgroundPosition = "center";
   card.append(front, back);
   cardContainer.append(card);
   gameboard.append(cardContainer);
 }
+
+let clickedCards = [];
+
 //add event listener to gameboard to listen for clicks
 gameboard.addEventListener("click", clicked);
 //if event.target is a card, then add class 'flipped' to event.target
 function clicked(e) {
-  console.log(e);
-  if (e.target.className === "front") {
-    //targeted the parent to effect all cards.
-    e.target.parentNode.classList.add("flipped");
+  if (clickedCards.length < 2) {
+    clickedCards.push(e.target.parentNode);
+    if (e.target.className === "front") {
+      //targeted the parent to effect all cards.
+      e.target.parentNode.classList.add("flipped");
+    }
+    if (clickedCards.length === 2) {
+      if (
+        clickedCards[0].getAttribute("data-card-id") ===
+        clickedCards[1].getAttribute("data-card-id")
+      ) {
+        console.log("issa match");
+        clickedCards = [];
+      } else {
+        for (let card of clickedCards) {
+          card.classList.remove("flipped");
+        }
+        clickedCards = [];
+      }
+    }
   }
 }
 
